c5a3b68eecedf7906e946bf7c09bf18c1e7af2ed
Daveloper87 <dai.williams@bbc.co.uk>
2015-02-19 23:57:03 +0000

    Set static folder content serving with empty css file

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var koa = require('koa');
 var app = koa();
+var serve = require('koa-static');
+
+app.use(serve(__dirname + '/public', {maxage: 0}));
 
 require('./app/conf/app.bootstrap')(app);
 require('./app/conf/app.routes')(app);
